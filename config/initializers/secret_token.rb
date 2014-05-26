a4b9902ee247197de65ab39e65c6246d3ece2c87
Christa Hartsock <christa.hartsock@gmail.com>
2014-05-26 10:25:20 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PLAYTest::Application.config.secret_key_base = '71c3a9d1fbd95661bf5a4d00b22a19b3f655257ec6cab86869a3b2d923726460b171c8efb2113a04bc32cba892a12d21301e17e03b389ef67104e1bedf9a38c7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PLAYTest::Application.config.session_store :cookie_store, key: '_pLAY_test_session'
